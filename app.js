1530e8e554121ce8769096b73c2afd4f0deb443a
Du <Sampan0103314@gmail.com>
2018-04-19 15:08:05 -0400

    adding Nunjucks

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const nunjucks = require('nunjucks');
 
 const app = express();
 
-// app.use((req,res,next) => {
-// 	morgan('tiny');
-// 	next();	
-// });
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
 app.use(morgan('tiny'));
 
 
 app.get('/', function(req, res){
-	res.send('Hello');
+	res.render('index', {title: 'An Example', people: people});
 	// console.log(req.method, '/', res.statusCode);
 });
 
