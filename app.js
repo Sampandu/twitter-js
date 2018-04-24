088db92a42894966a78df5eebadd780aefef2da6
Du <Sampan0103314@gmail.com>
2018-04-23 22:04:30 -0400

    add forms and http requests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const routes = require('./routes');
 const path = require('path');
 const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
 
 const app = express(); //// creates an instance of an express application
 
@@ -15,7 +16,8 @@ app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // app.use(morgan('dev'));
 app.use(volleyball);
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const router = express.Router();
 const tweetBank = require('../tweetBank');
 
 router.get('/', function(req, res){
-	let tweets = tweetBank.list();
-	res.render('index', { tweets: tweets});
+	let allOfTweets = tweetBank.list();
+	res.render('index', { 
+		tweets: allOfTweets,
+		showForm: true
+	});
 });
 
 
@@ -21,21 +24,31 @@ router.get('/', function(req, res){
   //   });
   // });
 
-	router.get('/users/:name', (req, res) => {
-		let name = req.params.name;
-		let tweetsForName = tweetBank.find({name: name});
-		res.render('index', {
-			tweets: tweetsForName
-		});
+router.get('/users/:name', (req, res) => {
+	let name = req.params.name;
+	let tweetsForName = tweetBank.find({name: name});
+	res.render('index', {
+		tweets: tweetsForName,
+		showForm: true,
+		userName: req.params.name
 	});
+});
 
-	router.get('/tweets/:id', (req, res, next) => {
-		console.log(req.params);
-		let tweetsForId = tweetBank.find({id: Number(req.params.id)}); //req.params返回的是字符串‘1’，‘2’...
-		res.render('index', {
-			tweets: tweetsForId
-		});
+router.get('/tweets/:id', (req, res, next) => {
+	console.log(req.params);
+	let tweetsForId = tweetBank.find({id: Number(req.params.id)}); //req.params返回的是字符串‘1’，‘2’...
+	res.render('index', {
+		tweets: tweetsForId
 	});
+});
+
+router.post('/tweets', (req, res, next)=>{
+	let name = req.body.name;
+	let text = req.body.text;
+	tweetBank.add(name, text);
+	res.redirect('/');
+});
+
 
 
 // 以下内容被app.js里面的express.static代替
